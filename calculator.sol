7e549aa9a957495ab346940cb3a111c4af01fb51
Alpharii <alphari404@gmail.com>
2025-03-29 16:02:13 +0700

    add calculator

--- calculator.sol
+++ calculator.sol
@@ -2,5 +2,25 @@
 pragma solidity ^0.8.0;
 
 contract Calculator {
-    
+    uint result = 0;
+
+    function add(uint a) public {
+        result += a;
+    }
+
+    function subtract(uint a) public {
+        result -= a;
+    }
+
+    function multiply(uint a) public {
+        result *= a;
+    }
+
+    function divide(uint a) public {
+        result /= a;
+    }
+
+    function getResult() public view returns (uint) {
+        return result;
+    }
 }
